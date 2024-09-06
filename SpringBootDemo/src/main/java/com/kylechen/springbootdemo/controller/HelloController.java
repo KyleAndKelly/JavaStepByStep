0f873e2a188d77c4010a81687fdb0d1230b1e9a5
KyleAndKelly <2431173627@qq.com>
2024-09-06 18:44:13 +0800

    test springBoot controller

--- SpringBootDemo/src/main/java/com/kylechen/springbootdemo/controller/HelloController.java
+++ SpringBootDemo/src/main/java/com/kylechen/springbootdemo/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.kylechen.springbootdemo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloController {
@@ -10,4 +9,40 @@ public class HelloController {
         System.out.println("hello world");
         return "hello world";
     }
+
+    @RequestMapping("/simpleParamTest")
+    public String simpleParamTest(@RequestParam( name = "",required = true) String name, Integer age){
+        System.out.println(name+":"+age);
+        return name+":"+age;
+    }
+
+
+
+    @RequestMapping("/pojoParamTest")
+    public String pojoParamTest(@RequestBody  Ueser user){
+        System.out.println(user.name);
+        System.out.println(user.age);
+        System.out.println(user.add.city);
+        System.out.println(user.add.street);
+        return "OK";
+    }
+
+    @RequestMapping("/path/{id}/{name}")
+    public String pathParamTest(@PathVariable Integer id,@PathVariable String name){
+        System.out.println(id+":"+name);
+        return "OK";
+    }
+
+
+}
+
+class Ueser{
+    public String name;
+    public Integer age;
+    public Address add;
+
+}
+class Address{
+    public String city;
+    public String street;
 }
