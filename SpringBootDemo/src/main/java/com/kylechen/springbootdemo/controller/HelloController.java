39c4c23edd99c1565792de92f9c355c686994537
KyleAndKelly <2431173627@qq.com>
2024-09-13 00:43:21 +0800

    test return Result class

--- SpringBootDemo/src/main/java/com/kylechen/springbootdemo/controller/HelloController.java
+++ SpringBootDemo/src/main/java/com/kylechen/springbootdemo/controller/HelloController.java
@@ -33,6 +33,17 @@ public class HelloController {
         return "OK";
     }
 
+    @RequestMapping(value = "/testResultObject")
+    public Result resultObjectTest(){
+        return new Result(0,"success",null);
+    }
+
+    @RequestMapping("/testResultObject1")
+    public Result resultObjectTest1(){
+        return Result.failed(null);
+    }
+
+
 
 }
 
@@ -46,3 +57,47 @@ class Address{
     public String city;
     public String street;
 }
+
+class Result{
+    private Integer code;
+    private String msg;
+    private Object data;
+    public  Result(){};
+    public  Result(Integer code,String msg,Object data){
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+
+    public  static  Result success(Object data){
+        return new Result(0,"success",data);
+    }
+    public  static  Result failed(Object data){
+        return new Result(1,"failed",data);
+    }
+}
+
